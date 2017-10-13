c5e2a703e88acf4707e2ac9698bc5318b5c5982d
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2017-10-13 12:08:39 +0300

    add JS_2 task

---
+++ JS_2/script.js
@@ -0,0 +1,31 @@
+//Фибоначчи
+var a = 0;
+var b = 1;
+var fibNumbers = [];
+for (var i = 0; i < 10; i++) {
+    var c = a + b;
+    fibNumbers.push(c);
+    a = b;
+    b = c;        
+}
+console.log(fibNumbers);
+
+//Факториал
+var res = 1;
+var n = 5; //факториал этого числа мы будем вычислять
+while(n>0){
+    res = res*n;
+    n--;
+}
+console.log(res);
+
+// Простые числа
+var simpNum = [];
+nextSimple:
+for(var i = 2; i < 20; i++){
+    for(var j=2; j < i ;j++){
+        if(i % j==0) continue nextSimple;
+    }
+    simpNum.push(i);
+}
+console.log(simpNum);
