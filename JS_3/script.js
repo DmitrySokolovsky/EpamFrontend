86d3e50a46b9235249cdf305fb0ef7933cb6a0ee
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2017-10-15 14:33:29 +0300

    fix:remove array from global environment

--- JS_3/script.js
+++ JS_3/script.js
@@ -1,16 +1,21 @@
-var argArray = [];
-function argFunc(arg) {    
-    if(arg){
-        for(var i = 0;i<arguments.length;i++){
-            argArray.push(arguments[i]);
-        }        
-        return null;
-    }
-    if(!arg){
-        console.log(argArray);
+function wrapArg() {
+    var argArray = [];
+
+    return function(arg) {
+        if (arg) {
+            for (var i = 0; i < arguments.length; i++) {
+                argArray.push(arguments[i]);
+            }
+            return null;
+        }
+        if (!arg) {
+            console.log(argArray);
+        }
     }
 }
 
-argFunc("String",1,2,3,3);
-argFunc("Epam","Front","end","training","2017","rulls",":-)");
+var argFunc = wrapArg();
+
+argFunc("String", 1, 2, 3, 3);
+argFunc("Epam", "Front", "end", "training", "2017", "rulls", ":-)");
 argFunc();
