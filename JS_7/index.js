7e03dbbb665f82bcced53f0005de6db2eff543aa
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2017-11-02 16:05:11 +0300

    FIXED!!!

--- JS_7/index.js
+++ JS_7/index.js
@@ -5,24 +5,24 @@ var url_3 = "http://www.nbrb.by/API/ExRates/Rates?Periodicity=0";
 var url_4 = "http://www.nbrb.by/API/ExRates/Rates?onDate=Sat%2C+28+Oct+2017+21%3A00%3A00+GMT&Periodicity=0";
 var url_5 = "http://www.nbrb.by/API/ExRates/Rates?onDate=Thu%2C+28+Sep+2017+21%3A00%3A00+GMT&Periodicity=1";
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
-function Start(url) {
+var x = 1;
+function Start(url,func) {
 
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", url, false);
-
+  xhr.open("GET", url, true);
   xhr.send();
-  if (xhr.status != 200) {
-    console.log(xhr.status + " " + xhr.statusText);
-  } else {
-    console.log(xhr.responseText + "\n end");
-  }  
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState == 4) {
+      if(xhr.status == 200) {
+        console.log(xhr.responseText + "\n "+ x+ "\n \n");
+        x++;
+      }
+    }  
+  } 
 }
-/*
-Start(url_1);
-Start(url_2);
-Start(url_3);
-Start(url_4);
-Start(url_5);
-*/
+
+Start(url_1,Start(url_2,Start(url_3,Start(url_4,Start(url_5,function(){
+  console.log("END");
+})))));
+
