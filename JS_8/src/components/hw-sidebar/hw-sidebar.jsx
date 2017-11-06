a6eebd2c749506917a72612c0113e521564840e7
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2017-11-06 15:08:34 +0300

    sidebar complete

--- JS_8/src/components/hw-sidebar/hw-sidebar.jsx
+++ JS_8/src/components/hw-sidebar/hw-sidebar.jsx
@@ -1,14 +1,51 @@
 import React,{Component} from 'react';
+import '../../css/font-awesome.min.css'
+import './hw-sidebar.css';
 
-export class SideBar extends Component{
+//fa fa-window-close-o
+class SideBar extends Component{
     constructor(props){
         super(props);
+        this.logosList = [
+            { 
+                className:'fa fa-film fa--padding',
+                name: 'Movies'
+            },
+            { 
+                className:'fa fa-television fa--padding',
+                name: 'TV Shows'
+            },
+            {
+                className:'fa fa-book fa--padding', 
+                name: 'My Library' 
+            },
+            {
+                className:'fa fa-user-circle fa--padding',
+                name: 'Support'
+            }
+        ];
     }
     render(){
-        (<div className="hw-side-bar">
-            <div className="hw-side-bar__logos-container">
-                
-            </div>
-        </div>)
+        return(
+            <div className="hw-side-bar hw-side-bar__container">
+                <div className="hw-side-bar__menu-logo">
+                <i className="fa fa-bars fa--padding"></i>
+                <i className="fa fa-window-close-o fa-2x fa--padding"></i>
+                <p className="hw-side-bar__text hw-side-bar__text--big">Logo</p>
+                </div>
+                <div className="hw-side-bar__menu-container">
+                <div className="hw-side-bar__vertical-container">
+                {this.logosList.map((item)=>{
+                    return (
+                        <div className="hw-side-bar__menu-item">
+                        <i className={item.className}></i><p className="hw-side-bar__text">{item.name}</p>
+                        </div>
+                    );
+                })}
+                </div>
+                </div>
+        </div>);
     }
 }
+
+export default SideBar;
--- JS_8/src/main.js
+++ JS_8/src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import SideBar from './components/hw-sidebar/hw-sidebar.jsx'
 import './style.css';
 
+ReactDom.render(<SideBar/>,document.getElementById('test'));
 
 
