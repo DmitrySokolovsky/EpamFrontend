99ceae154df951b1325f5f0e816b56a0cc824036
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2017-11-06 23:34:52 +0300

    add header as container

--- JS_8/src/components/hw-header/hw-header.jsx
+++ JS_8/src/components/hw-header/hw-header.jsx
@@ -2,5 +2,14 @@ import React,{Component} from 'react';
 import './hw-header.css';
 
 export default class Navigation extends Component{
-    
+    constructor(props){
+        super(props);
+    };
+    render(){
+        return (
+            <div className="hw-header">
+                <header></header>
+            </div>
+        );
+    }
 }
--- JS_8/src/main.js
+++ JS_8/src/main.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SideBar from './components/hw-sidebar/hw-sidebar.jsx'
+import Header from './components/hw-header/hw-header.jsx';
 import './style.css';
 
-ReactDom.render(<SideBar/>,document.getElementById('test'));
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    };
+    render(){
+        return(
+            <div id="test">
+                <SideBar/>
+                <Header/>
+            </div>            
+        );
+    }
+
+}
+
+ReactDom.render(<App/>,document.getElementById('test'));
 
 
