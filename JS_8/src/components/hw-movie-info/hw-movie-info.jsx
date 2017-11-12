a2d1ae99a608dabe7b0442179caea5a25ecb9926
Dmitry Sokolovsky <assholedrummer665@yandex.ru>
2017-11-12 11:56:42 +0300

    add movie-info component without  css

--- JS_8/src/components/hw-movie-info/hw-movie-info.jsx
+++ JS_8/src/components/hw-movie-info/hw-movie-info.jsx
@@ -9,7 +9,26 @@ class MovieInfo extends Component{
     render(){
         return (
             <div className="hw-movie-info hw-movie-info__container">
-                
+                <header>
+                    <div className="hw-header__container">                
+                        <Navigation hideForm={true}/>                                        
+                    </div>
+                </header>
+                <div className="hw-movie-info__description-container">
+                    <div className="hw-movie-info__poster-container"></div>
+                    <div className="hw-movie-info__overview-container">
+                        <h1 className="hw-movie-info__text"></h1>
+                        <p className="hw-movie-info__text"></p>
+                    </div>
+                </div>
+                <div className="hw-movie-info__genre-container">
+                    <h3 className="hw-movie-info__text">Genres</h3>
+                    <div className="hw-movie-info__genres-list"></div>
+                </div>
+                <div className="hw-movie-info__recomindation-container">
+                    <h3 className="hw-movie-info__text">We also recommend</h3>
+                    <div className="hw-movie-info__recomindation-list"></div>
+                </div>
             </div>
         );
     }
